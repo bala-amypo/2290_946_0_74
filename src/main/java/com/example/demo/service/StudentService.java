a7462ae0b77b1eb88793655f6be3081e6d9f25be
bala-amypo <bala@amypo.in>
2025-12-18 08:00:39 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,17 +1,15 @@
 package com.example.demo.service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+
 
 import com.example.demo.entity.Student;
 
+
 public interface StudentService {
-
+  
     Student createData(Student stu);
-
     List<Student> fetchRecord();
-
-    Optional<Student> fetchDataById(int id);
-
-    void deleteData(int id);
+     Optional<Student> fetchDataById(int id);   
+     void deleteData(int id);
 }
